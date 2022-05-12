ee996a9d3596b70b6fffbef2d00b2b7f59920500
Michele Schimd <michele.schimd@itiszuccante.edu.it>
2022-05-12 21:59:01 +0200

    SdI02 end notebook. Minor changes.

--- 02_pytho_strutture_dati/liste_indici.py
+++ 02_pytho_strutture_dati/liste_indici.py
@@ -3,7 +3,7 @@ print(lista[0:3]) # elementi dalla posizione 0 (inclusa) alla 3 (esclusa) [0, 1,
 print(lista[:3]) # lo stesso (quando l'inizio è 0 si può omettere)
 print(lista[2:4]) # elementi dalla posizione 2 (inclusa) alla 4 (esclusa) [2, 3]
 print(lista[3::]) # elementi dalla posizione 3 alla fine [3, 4, 5, ..., 9]
-print(lista[::]) # stampa tutta la lista
+print(lista[:]) # stampa tutta la lista (anche lista[::])
 print(lista[0:5:2]) # da posizione 0 a 5 (esclusa) a salti di due [0, 2, 4]
 print(lista[::3]) # tutta la lista a salti di 3 [0, 3, 6, 9]
 print(lista[-1]) # 9 (no lista, ma numero!)
